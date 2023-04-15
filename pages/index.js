e2b9995fc7d8909b60288cc7241f81110233392d
FranLpz <franciscoalopezd@gmail.com>
2023-04-15 18:45:44 -0500

    Add user input and generate button

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
+import { useState } from "react";
+
 const Home = () => {
+
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (event) => {
+    // console.log(event.target.value);
+    setUserInput(event.target.value);
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,10 +21,25 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Historical Events in the words of his protagonist!</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Ask about any historical event and get a resume in the own words of their protagonist.</h2>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="your text here" 
+            className="prompt-box" 
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
