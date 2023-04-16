4306806bbbab6ac4e965044268650098408b1c32
FranLpz <franciscoalopezd@gmail.com>
2023-04-15 22:41:19 -0500

    Add base prompt with logic and fix some styles

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,16 +6,21 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
+const basePromptPrefix = 
+`
+Write a resume of this historical event like one of his main historical protagonists, make sure of include in the narrative who’s writing the resume, location, death toll, winners and post events.
+
+Historical Event:
+`;
 const generateAction = async (req, res) => {
     // Run first prompt
-    console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`);
 
     const baseCompletion = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: `${basePromptPrefix}${req.body.userInput}`,
+        prompt: `${basePromptPrefix}${req.body.userInput}\n`,
         temperature: 0.7,
-        max_tokens: 250,
+        max_tokens: 500,
     });
 
     const basePromptOutput = baseCompletion.data.choices.pop();
